02f66fb2e1d1d689fdbb2a38a66b3a84f3fc4076
Simeon <drsimeon_87@abv.bg>
2020-10-11 18:20:07 +0300

    added javadoc to Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,8 +8,12 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Main class of the application where the starting and initialization of the application occurs.
+ */
 public class Application {
     private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
 
 
@@ -23,16 +27,22 @@ public class Application {
             User loggedUser = userController.getUserByUsername(username);
             loggedUsers.add(loggedUser);
             System.out.println("User " + username + " logged in successfully.");
-        }else {
+        } else {
             System.out.println("Provided username and password does not match any existing account.");
         }
-
     }
 
+    /**
+     * Uses BufferedReader to get the user input from the console when called.
+     * Throws custom exception when the BufferedReader throws an IOException.
+     *
+     * @return String representation of the user input
+     * or Throws CustomException.
+     */
     private static String getUserInput() {
         try {
             return bufferedReader.readLine();
-        }catch (IOException exception){
+        } catch (IOException exception) {
             throw new CustomException(exception.getMessage());
         }
     }
