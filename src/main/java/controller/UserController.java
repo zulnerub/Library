4596a6357f3492036aac6441e33d91d1982f4891
Simeon <drsimeon_87@abv.bg>
2020-10-11 13:12:12 +0300

    added validion for loging and registering users in userController class

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import enums.Gender;
+import model.common.Address;
 import model.user.impl.User;
 import repository.UserRepository;
 
@@ -22,30 +24,67 @@ public class UserController {
         return false;
     }
 
-    public String registerUser(String username, String password) {
-        if (isUsernameValid(username)) {
-            User user = new User(username, password)
-            userrepository.addUser();
-        } else {
-            return "Username not valid. Username must be at least 8 symbols long.";
+    public String registerUser(String username, String password, boolean GDPR,
+                               String firstName, String lastName,
+                               int age, Address address, Gender gender, String email) {
+        if (!GDPR){
+            return "Please consent to the GDPR agreement.";
         }
 
+        if (!isUsernameValid(username)) {
+            return "Username is not valid. Username must be at least 8 symbols long.";
+        }
+
+        if (!isPasswordValid(password)){
+            return "Password is not invalid. Password must be at least 5 symbols long";
+        }
+
+        if (!isNameValid(firstName) || !isNameValid(lastName)){
+            return "The specified first and last name must be long at least 8 symbols each.";
+        }
+
+        if (!isAgeValid(age)){
+            return "Specified age must be between 6 and 125 - excluded.";
+        }
+
+        if (!isGenderValid(gender)){
+            return "The provided options for gender are: 'm' or 'f'.";
+        }
+
+        if (!isAddressValid(address)){
+            return "Please provide a valid address. Fields must be non empty.";
+        }
+
+        if (!isEmailValid(email)){
+            return "Please provide a valid email.";
+        }
+
+        User user = new User(firstName, lastName, address,
+                gender, username, password, email, GDPR, age);
+
+        userrepository.addUser(user);
+
+        return "User " + username + " registered Successfully.";
     }
 
-    private boolean isEmailValid(String email){
+    private boolean isAddressValid(Address address) {
+        return address.isAddressValid();
+    }
+
+    private boolean isGenderValid(Gender gender) {
+        return gender.name().equals("m") || gender.name().equals("f");
+    }
+
+    private boolean isEmailValid(String email) {
         return Pattern.matches(
                 "^[a-z][a-zA-Z0-9_.]*@([a-z][a-zA-Z0-9_]*(\\.))+[a-zA-Z]+", email);
     }
 
-    private boolean validateGDPRConsent(boolean GDPRConsent){
-        return GDPRConsent;
+    private boolean isNameValid(String name) {
+        return isStringInputValid(name) && name.trim().length() > 1 && name.trim().length() <= 20;
     }
 
-    private boolean isNameValid(String name){
-        return name != null && name.trim().length() > 1 && name.trim().length() <= 20;
-    }
-
-    private boolean isAgeValid(int age){
+    private boolean isAgeValid(int age) {
         return age > 6 && age < 125;
     }
 
--- src/main/java/model/common/Address.java
+++ src/main/java/model/common/Address.java
@@ -3,19 +3,35 @@ package model.common;
 public class Address {
     private String country;
     private String city;
-    private String address;
+    private String street;
 
-    public Address(String country, String city, String address) {
+    public Address(String country, String city, String street) {
         this.country = country;
         this.city = city;
-        this.address = address;
+        this.street = street;
     }
 
     public String getFullAddress(){
-        return address + ", " + city + ", " + country;
+        return street + ", " + city + ", " + country;
     }
 
     public String getShortAddress(){
-        return address;
+        return street;
+    }
+
+    public boolean isAddressValid(){
+        return isCountryValid() && isCityValid() && isStreetValid();
+    }
+
+    private boolean isStreetValid() {
+        return street != null && !street.isBlank();
+    }
+
+    private boolean isCityValid() {
+        return city != null && !city.isBlank();
+    }
+
+    private boolean isCountryValid() {
+        return country != null && !country.isBlank();
     }
 }
