34e905deb3e9d17c325791f308d243064131c470
Simeon <drsimeon_87@abv.bg>
2020-10-11 17:49:05 +0300

    added java doc to UserRepository.class

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -5,24 +5,45 @@ import model.user.impl.User;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Has the role of containing all the registered users and execute simple operations like:
+ * - adding user;
+ * - searching for user
+ * - getting all users
+ * - etc.
+ */
 public class UserRepository {
     private List<User> users = new ArrayList<>();
 
     public UserRepository() {
     }
 
-    public void addUser(User user){
+    /**
+     * Adds a User object to the collection of users.
+     *
+     * @param user Object of type User validated in advance.
+     */
+    public void addUser(User user) {
         users.add(user);
     }
 
-    public User getUser(String username){
+    /**
+     * Searches for a user by username.
+     *
+     * @param username String representation of the user's name.
+     * @return if found returns the found User otherwise - null.
+     */
+    public User getUser(String username) {
         return users.stream()
                 .filter(user -> user.getUsername().equals(username))
                 .findFirst()
                 .orElse(null);
     }
 
-    public List<User> getAllUsers(){
+    /**
+     * @return Gets all the users in the repository.
+     */
+    public List<User> getAllUsers() {
         return users;
     }
 }
