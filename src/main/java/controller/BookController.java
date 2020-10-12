be783e0764fa1ce82716c38dd1f49688278db015
Simeon <drsimeon_87@abv.bg>
2020-10-12 11:15:07 +0300

    added BookController class

---
+++ src/main/java/controller/BookController.java
@@ -0,0 +1,158 @@
+package controller;
+
+import enums.BookCategory;
+import enums.BookGenre;
+import model.user.impl.Author;
+import repository.BookRepository;
+
+import java.util.List;
+
+/**
+ * Creates connection between the user and the book repository.
+ * Provides validation for the books before added to the library.
+ * Operates on the book repository.
+ */
+public class BookController {
+    private final BookRepository bookRepository = new BookRepository();
+
+    public BookController() {
+    }
+
+    /**
+     * Calls methods to validate separately each of the parameters and
+     * returns an error message whe the first invalid method is found.
+     *
+     * @param bookISBN       Unique book identification.
+     * @param bookTitle      String representation of the title of the book.
+     * @param summary        Short info describing the book.
+     * @param authors        List of objects of type Author with length at least one.
+     * @param bookGenres     List of objects of type BookGenre with length at least one.
+     * @param bookCategories List of objects of type BookCategory with length at least one.
+     * @return true if all parameters are valid, otherwise on the first invalid operator
+     * prints error message and returns false.
+     */
+    public boolean isBookValid(String bookISBN, String bookTitle, String summary,
+                               List<Author> authors, List<BookGenre> bookGenres, List<BookCategory> bookCategories) {
+        if (!isISBNValid(bookISBN)) {
+            System.out.println(("The provided ISBN was not valid. " +
+                    "The ISBN should consist of 5 numbers and a '-' symbol in the format '####-#'."));
+            return false;
+        }
+
+        if (!isTitleValid(bookTitle)) {
+            System.out.println("The provided book title is not valid. " +
+                    "The title should be at least 3 symbols long.");
+            return false;
+        }
+
+        if (!isSummaryValid(summary)) {
+            System.out.println("The provided summary is not valid. " +
+                    "The summary must be at least 50 symbols in length.");
+            return false;
+        }
+
+        if (!areAuthorsValid(authors)) {
+            System.out.println("The provided author/authors is/are not valid. " +
+                    "Please provide at least one, valid author to add a book to the library.");
+            return false;
+        }
+
+        if (!areBookGenresValid(bookGenres)) {
+            System.out.println("The provided genre/s of the book are not valid. " +
+                    "Please provide at least one valid book genre.");
+            return false;
+        }
+
+        if (!areBookCategoriesValid(bookCategories)) {
+            System.out.println("The provided book category/ies are not valid. " +
+                    "Please provide at least one valid category to add a book to the library.");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates the provided categories for the book to be added.
+     *
+     * @param bookCategories Should be list of objects of type BookCategory with length at least 1.
+     * @return true if provided list exists and has at least one category in it, otherwise - false.
+     */
+    private boolean areBookCategoriesValid(List<BookCategory> bookCategories) {
+        return bookCategories != null && !bookCategories.isEmpty();
+    }
+
+    /**
+     * Validates the provided list of BookGenres.
+     *
+     * @param bookGenres Should be A list with objects of type BookGenre.
+     * @return true if there is at least one genre in the list, otherwise - false.
+     */
+    private boolean areBookGenresValid(List<BookGenre> bookGenres) {
+        return bookGenres != null && !bookGenres.isEmpty();
+    }
+
+    /**
+     * Validates the provided list of authors.
+     *
+     * @param authors List of objects of type Author with length at least 1.
+     * @return true if list is not null and not empty, otherwise - false.
+     */
+    private boolean areAuthorsValid(List<Author> authors) {
+        return authors != null && !authors.isEmpty();
+    }
+
+    /**
+     * Validates the provided input for summary.
+     *
+     * @param summary Should be a string longer than 50 symbols.
+     * @return true if the provided string is valid or false if not.
+     */
+    private boolean isSummaryValid(String summary) {
+        return isStringValid(summary) && summary.length() > 50;
+    }
+
+    /**
+     * Validates the provided  input if it is a valid title.
+     *
+     * @param bookTittle Should be a string longer than 3 symbols.
+     * @return true if valid, otherwise - false
+     */
+    private boolean isTitleValid(String bookTittle) {
+        return isStringValid(bookTittle) && bookTittle.length() > 3;
+    }
+
+    /**
+     * Validates the provided ISBN for the new book.
+     *
+     * @param bookISBN String representation of the ISBN - should be in format "####-#".
+     * @return true if input is valid otherwise -false.
+     */
+    private boolean isISBNValid(String bookISBN) {
+        boolean result = false;
+
+        if (isStringValid(bookISBN) && bookISBN.trim().length() == 6
+                && bookISBN.trim().charAt(4) == '-') {
+            result = true;
+
+            for (int i = 0; i < 6; i++) {
+                if (i != 4) {
+                    if (!Character.isDigit(bookISBN.trim().charAt(i))) {
+                        result = false;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Validates the provided input.
+     *
+     * @param strToValidate Should be a string.
+     * @return true if the provided input is not null and not blank, otherwise returns false.
+     */
+    private boolean isStringValid(String strToValidate) {
+        return strToValidate != null && !strToValidate.isBlank();
+    }
+}
